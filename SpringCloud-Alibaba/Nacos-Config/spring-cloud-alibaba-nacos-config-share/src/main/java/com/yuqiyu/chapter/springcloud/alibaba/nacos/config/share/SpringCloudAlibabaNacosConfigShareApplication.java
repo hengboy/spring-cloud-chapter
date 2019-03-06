352db0d6b6ae74761a47f5a806fb8b442187e24b
恒宇少年 <jnyuqy@dingtalk.com>
2019-03-06 16:48:04 +0800

    SpringCloud Alibaba Nacos Config 共享配置

--- SpringCloud-Alibaba/Nacos-Config/spring-cloud-alibaba-nacos-config-share/src/main/java/com/yuqiyu/chapter/springcloud/alibaba/nacos/config/share/SpringCloudAlibabaNacosConfigShareApplication.java
+++ SpringCloud-Alibaba/Nacos-Config/spring-cloud-alibaba-nacos-config-share/src/main/java/com/yuqiyu/chapter/springcloud/alibaba/nacos/config/share/SpringCloudAlibabaNacosConfigShareApplication.java
@@ -1,12 +1,7 @@
 package com.yuqiyu.chapter.springcloud.alibaba.nacos.config.share;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  * SpringCloud Alibaba Nacos Config Share
@@ -14,33 +9,9 @@ import org.springframework.web.bind.annotation.RestController;
  * @author yuqiyu
  */
 @SpringBootApplication
-@RestController
-@RefreshScope
-@RequestMapping(value = "/config")
 public class SpringCloudAlibabaNacosConfigShareApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringCloudAlibabaNacosConfigShareApplication.class, args);
-    }
-
-    /**
-     * hengboy-sca-nacos-config-share.yaml配置内容
-     */
-    @Value(value = "${share.basic:}")
-    private String basic;
-    /**
-     * hengboy-common.yaml配置内容
-     */
-    @Value(value = "${common.version:}")
-    private String commonVersion;
-    /**
-     * hengboy-service.yaml配置内容
-     */
-    @Value(value = "${service.order:}")
-    private int serviceOrder;
-
-    @GetMapping(value = "/get")
-    public String getConfig() {
-        return basic + "," + commonVersion + "," + serviceOrder;
     }
 }
